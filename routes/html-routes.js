b5c94b616f3f91f14d5a776826f7b1bd923dac68
Justin Besteman <justin@besteman.io>
2017-02-27 10:29:41 -0500

    made foundation folders and files. Also, made server.js logic

---
+++ routes/html-routes.js
@@ -0,0 +1,14 @@
+var db = require('../models');
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/index.html"));
+
+    });
+};
